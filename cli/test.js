5474c9e8a316166052f1ba99c7f053827c331640
Thales Avila <thalespizza@gmail.com>
2019-02-02 18:21:55 -0200

    Manipulando arquivos READ

--- cli/test.js
+++ cli/test.js
@@ -9,11 +9,18 @@ const DEFAULT__ITEM__CADASTRAR = {
   id: 1
 }
 
+const database = require('./database')
+
 describe('Suite de manipulação de Herois', function() {
-    it('Deve cadastrar um heroi, usando arquivo', async () => {
+    it('Deve pesquisar um heroi usando arquivos', async () => {
+      const expected = DEFAULT__ITEM__CADASTRAR
+      const [resultado] = await database.listar(expected.id)
+      deepEqual(resultado, expected)
+    })  
+  /* it('Deve cadastrar um heroi, usando arquivo', async () => {
       const expected = DEFAULT__ITEM__CADASTRAR
 
       //
       ok(null, expected)
-    })
+    }) */
 })
