114d2babcc264408cea3a994cdfa2653f9f4252d
Thales Avila <thalespizza@gmail.com>
2019-02-02 08:05:59 -0200

    service craido

---
+++ testes/01/tests.js
@@ -0,0 +1,10 @@
+const assert = require('assert') 
+
+describe('Star Wars Tests', () => {
+  it('Deve buscar o r2d2 com o formato correto', async () => {
+    const expected = [{ nome: 'R2-D2', peso: '96' }]
+    const nomeBase = `r2-d2`
+  })
+})
+
+assert.ok(false)
