4a072b19d2c9bc143a335df2a20e5f4bfc08a9b0
Thales Avila <thalespizza@gmail.com>
2019-02-13 00:11:48 -0200

    Deleçao de Heroi CLI

--- cli/index.js
+++ cli/index.js
@@ -6,16 +6,20 @@ async function main() {
     .version('v1')
     .option('-n, --nome [value]', "Nome do Heroi")
     .option('-p, --poder [value]', "Poder do Heroi")
+    .option('-i, --id [value]', "ID do Heroi")
 
     .option('-c, --cadastrar', "Cadastrar Heroi")
     .option('-l, --listar', "Listar um Heroi")
+    .option('-r, --remover [value]', "Remove um Heroi pelo id")
+    .option('-a, --atualizar [value]', "Atualizar um Heroi pelo id")
 
     .parse(process.argv)
 
     const heroi = new Heroi(Commander)
 
     try {
-      if(Commander.cadastrar) {      
+      if(Commander.cadastrar) {
+        delete heroi.id      
         const resultado = await Database.cadastrar(heroi)
         if(!resultado) {
           console.error('Heroi não foi encontrado!')
@@ -28,8 +32,18 @@ async function main() {
         const resultado = await Database.listar()
         console.log(resultado);
         return;
-        
-      }  
+      }
+      if(Commander.remover) {
+        const resultado = await Database.remover(heroi.id)
+        if(!resultado) {
+          console.error('Não foi possível remover o heroi!')
+          return;
+        }
+        console.log('Heroi removido com sucesso!')
+      }
+      if(Commander.atualizar) {
+        const idParaAtualizar = parseInt(Commander.atualizar)
+      }
     } catch(error) {
       console.error('DEU RUIM', error)
     }
