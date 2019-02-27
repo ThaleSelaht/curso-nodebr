b25adcafb6b9008c6363f3e6fca1237f71bd010e
Thales Avila <thalespizza@gmail.com>
2019-02-26 21:43:35 -0300

    Estrategias de BD e Interface implementadas,

--- multi-db/src/strategy.js
+++ multi-db/src/strategy.js
@@ -18,3 +18,40 @@ class ICrud {
     throw new NotImplementedException()
   }
 }
+// Implementando as estratégias
+class MongoDB extends ICrud{
+  constructor(){
+    super()
+  }
+  create(item){
+    console.log('O item foi salvo em MongoDB')
+  }
+}
+class Postgres extends ICrud{
+  constructor(){
+    super()
+  }
+  create(item){
+    console.log('O item foi salvo em Postgres')
+  }
+}
+
+//Implementando a interface
+class ContextStrategys{
+  constructor(strategy) {
+    this._database = strategy
+  }
+
+  create(item) {
+    return this._database.create(item)
+  }
+  read(item) {
+    return this._database.read(item)
+  }
+  update(id, item) {
+    return this._database.update(id, item)
+  }
+  delete(id){
+    return this._database.delete(id)
+  }
+}
